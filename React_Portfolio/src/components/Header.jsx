f835854da7cf9abe18f44b9ba73db95039e3ed85
Shane Mindreau <smindre1@binghamton.edu>
2023-12-11 15:18:17 -0500

    I added a basic components file structure and filled in the Header.jsx file based on my prior portfolio site.

---
+++ React_Portfolio/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+    <header>
+    <h1>Shane Mindreau</h1>
+    <nav class="menu">
+        <a href="#About-Me-Path" class="menuBarText">About Me</a>
+        <a href="#My-Work-Path" class="menuBarText">My Work</a>
+        <a href="#Contact-Me-Path" class="menuBarText">Contact Me</a>
+        {/* I have yet to make a Resume webpage so it is not linked */}
+        <a href="#Contact-Me-Path" class="menuBarText">Resume</a>
+    </nav>
+    </header>
+    );
+  }
+  
+  export default Header;
