0f27028e455853d1c696cd030f99d10d6c39597b
Shane Mindreau <smindre1@binghamton.edu>
2024-01-22 18:23:53 -0500

    I added a feature to the header which will highlight the header <a> link according to what page the user is currently on. This involved adding the hooks useState and useEffect, as well as some additional CSS for the highlight.

--- React_Portfolio/src/components/Header.jsx
+++ React_Portfolio/src/components/Header.jsx
@@ -1,12 +1,34 @@
+import { useState, useEffect} from "react";
+
 function Header() {
+    const [about, setAbout] = useState(false);
+    const [portfolio, setPortfolio] = useState(false);
+    const [contact, setContact] = useState(false);
+    const [resume, setResume] = useState(false);
+
+
+    const pageSelection = () => {
+        const page = window.location.pathname;
+        page == "/" ? setAbout(true) : setAbout(false);
+        page == "/Portfolio" ? setPortfolio(true) : setPortfolio(false);
+        page == "/Contacts" ? setContact(true) : setContact(false);
+        page == "/Resume" ? setResume(true) : setResume(false);
+    };
+
+    useEffect(() => {
+        pageSelection();
+        console.log("run");
+    }, [about, portfolio, contact, resume])
+    
+    // {about ? "highlight menuBarText" : "menuBarText"}
     return (
     <header>
     <h1 className="logo"><a href="/">SM</a></h1>
     <nav className="menu">
-        <a href="/" className="menuBarText">About Me</a>
-        <a href="/Portfolio" className="menuBarText">Portfolio</a>
-        <a href="/Contacts" className="menuBarText">Contacts</a>
-        <a href="/Resume" className="menuBarText">Resume</a>
+        <a href="/" className={about ? "highlight menuBarText" : "menuBarText"}>About Me</a>
+        <a href="/Portfolio" className={portfolio ? "highlight menuBarText" : "menuBarText"}>Portfolio</a>
+        <a href="/Contacts" className={contact ? "highlight menuBarText" : "menuBarText"}>Contacts</a>
+        <a href="/Resume" className={resume ? "highlight menuBarText" : "menuBarText"}>Resume</a>
     </nav>
     </header>
     );
