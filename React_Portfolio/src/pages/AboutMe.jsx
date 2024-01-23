3081e13d46ec5ac8e259b4592a32f9244413859e
Shane Mindreau <smindre1@binghamton.edu>
2024-01-22 23:17:17 -0500

    I filled in the AboutMe page with a proper bio and I tried to format the CSS of it well. It still needs a bit more refining, and the profile picture is still in the works. But it looks good.

--- React_Portfolio/src/pages/AboutMe.jsx
+++ React_Portfolio/src/pages/AboutMe.jsx
@@ -6,8 +6,17 @@ function AboutMe() {
       <article className="flexColumn bio">
         <h2 className="name">Shane Mindreau</h2>
         <div>
-          <p>N/A</p>
-          <p>N/A</p>
+          <h3 className="subtitle">Full-Stack Web Developer</h3>
+          <p className="bodyText">Welcome! I'm Shane, a full-stack web developer with a passion for building creative and reactive digital projects. I am specialized in using React.js to make dynamic front-end designs as well as MongoDB/SQL databases for robust data management.</p>
+          <h3 className="subtitle">I'm Also Proficient In...</h3>
+          <li className="bodyText">- Node.js</li>
+          <p className="bodyText">- MERN stack</p>
+          <p className="bodyText">- RESTful APIs</p>
+          <p className="bodyText">- GraphQL APIs</p>
+          <p className="bodyText bulletPoint">*** And more in my Resume section!</p>
+          <h3 className="subtitle">Education</h3>
+          <p className="bodyText">Full-Stack Certificate, Colombia University Coding Bootcamp</p>
+          <p className="bodyText">BA Graphic Design, Binghamton University</p>
         </div>
       </article>
       <figure>
