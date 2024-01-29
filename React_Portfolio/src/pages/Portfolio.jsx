95e77cfd4e1d69b947cc1beb2e0839440e2ab68c
Shane Mindreau <smindre1@binghamton.edu>
2024-01-28 23:09:07 -0500

    I have reworked the portfolio page, first by switching out the displayed projects. I reworked the appearance of the controls for flipping between projects by changing the next and previous buttons to stylized svg arrows, as well as including icons between these buttons that hint towards the previous, current, and next project themes. I stylized the call to action buttons and fixed their functions so now when they are clicked they will open a new tab for that project's repository or deployed site accordingly. Lastly I adjusted the display animation and updated their descriptions.

--- React_Portfolio/src/pages/Portfolio.jsx
+++ React_Portfolio/src/pages/Portfolio.jsx
@@ -2,6 +2,11 @@ import Project from "../components/Project";
 import Path1 from "../assets/images/song-finder-custom-image.png"
 import Path2 from "../assets/images/Present-List-Custom-Image.png"
 import Path3 from "../assets/images/project-image-placeholder.png"
+import PawIcon from "../assets/images/icons/Paw_Icon.svg"
+import WeatherIcon from "../assets/images/icons/Weather_Icon.svg"
+import QuizIcon from "../assets/images/icons/Quiz_Icon.svg"
+import Arrow from "../assets/images/Arrow.svg"
+
 import { useState, useRef, useEffect } from "react";
 
 const Portfolio = () => {
@@ -12,60 +17,60 @@ const Portfolio = () => {
   const textBody = useRef(null);
 
   const projects = [
+    // {
+    //   href: "https://smindre1.github.io/Song-Finder/",
+    //   repo: "https://github.com/smindre1/Song-Finder",
+    //   image: Path1,
+    //   alt: "Song Finder Display Image",
+    //   icon: "",
+    //   projectTitle: "Song Finder",
+    //   desc: "Code Used: HTML, CSS, & JS",
+    //   purpose: "",
+    //   techStack: "",
+    // },
+    // {
+    //   href: "https://present-list-fc60525bdd4f.herokuapp.com/",
+    //   repo: "https://github.com/smindre1/Present-List?tab=readme-ov-file",
+    //   image: Path2,
+    //   alt: "Present List Display Image",
+    //   icon: "",
+    //   projectTitle: "Present List",
+    //   desc: "Code Used: HTML, CSS, JS, SQL",
+    //   purpose: "",
+    //   techStack: "",
+    // },
     {
-      id: 1,
-      href: "https://smindre1.github.io/Project-One-Front-End-Dev/",
-      repo: "",
-      image: Path1,
-      alt: "Project 1 Background",
-      projectTitle: "Song Finder",
-      desc: "Code Used: HTML, CSS, & JS",
-      mainImg: true,
-      caption: true,
-    },
-    {
-      id: 2,
-      href: "https://present-list-fc60525bdd4f.herokuapp.com/",
-      repo: "",
-      image: Path2,
-      alt: "Project 2 Background",
-      projectTitle: "Present List",
-      desc: "Code Used: HTML, CSS, JS, SQL",
-      mainImg: false,
-      caption: false,
-    },
-    {
-      id: 3,
-      href: "https://github.com/smindre1/pacey",
-      repo: "",
+      href: "https://pawsandfound-dd3dbebb5f23.herokuapp.com/",
+      repo: "https://github.com/smindre1/PawsAndFound",
       image: Path3,
-      alt: "Project 3 Background",
-      projectTitle: "Project Name 3:",
-      desc: "Code Languages Used",
-      mainImg: false,
-      caption: false,
+      alt: "Paws And Found Display Image",
+      icon: PawIcon,
+      projectTitle: "Paws And Found",
+      desc: "This is a reactive MERN application using Apollo to run GraphQL API queries on our backend server.",
+      purpose: "Me and my collaborators have created a lost and found pet blog in which users can post and reply about animals they have lost or found.",
+      techStack: "",
     },
     {
-      id: 4,
-      href: "https://github.com/smindre1/pacey",
-      repo: "",
+      href: "https://smindre1.github.io/Weather-App/",
+      repo: "https://github.com/smindre1/Weather-App",
       image: Path3,
-      alt: "Project 4 Background",
-      projectTitle: "Project Name 4:",
-      desc: "Code Languages Used",
-      mainImg: false,
-      caption: false,
+      alt: "Weather App Display Image",
+      icon: WeatherIcon,
+      projectTitle: "Weather App",
+      desc: "This is a weather app that takes a User's searched location and provides that location's current weather conditions and a five day forecast for the location.",
+      purpose: "",
+      techStack: "",
     },
     {
-      id: 5,
-      href: "https://github.com/smindre1/pacey",
-      repo: "",
+      href: "https://smindre1.github.io/quiz-game/",
+      repo: "https://github.com/smindre1/quiz-game",
       image: Path3,
-      alt: "Project 5 Background",
-      projectTitle: "Project Name 5",
-      desc: "Code Languages Used",
-      mainImg: false,
-      caption: false,
+      alt: "Quiz Game Display Image",
+      icon: QuizIcon,
+      projectTitle: "Quiz Game",
+      desc: "This is a program that creates a multiple choice timed quiz. It gives 100 points for each correct answer and deducts time from the timer for every incorrect answer. When the user either runs out of time or questions the program requests initials to tie to the attempt's score.",
+      purpose: "",
+      techStack: "",
     },
   ];
   const range = projects.length - 1;
@@ -74,11 +79,11 @@ const Portfolio = () => {
   @keyframes fadeIn {
     from {
       opacity: 0;
-      transform: translateX(0%);
+      transform: translateX(-20%);
     }
     to {
       opacity: 1;
-      transform: translateX(-123px);
+      transform: translateX(-30%);
     }
   }
 `;
@@ -94,7 +99,6 @@ const Portfolio = () => {
   }
   `;
 
-
   useEffect(() => {
     
     oldMain.current.classList.add("vanish");
@@ -102,43 +106,15 @@ const Portfolio = () => {
     
     textBody.current.style.animation = 'textAppear 2s';
     textBody.current.style.animationFillMode = 'forwards';
-    newMain.current.style.animation = 'fadeIn 2s';
+    newMain.current.style.animation = 'fadeIn 1.25s';
     newMain.current.style.animationFillMode = 'forwards';
     setTimeout(() => {oldMain.current.classList.remove("vanish");
     newMain.current.classList.add("hide"); textBody.current.style.animation = '';}, 2000)
     
+
+    
   }, [display]);
 
-  // const rightEnterAnimation = () => {
-  //   // const targetCord = oldMain.current.getBoundingClientRect();
-  //   // console.log("target", targetCord);
-    
-
-  //   // oldMain.current.classList.add("hide");
-  //   oldMain.current.classList.add("vanish");
-  //   // textBody.current.classList.add("vanish");
-
-  //   newMain.current.classList.remove("hide");
-  //   // const actualCord = newMain.current.getBoundingClientRect();
-  //   // console.log("actual", actualCord);
-  //   newMain.current.classList.add("fade-in");
-  //   range != text ? setText(text + 1) : setText(0);
-  //   textBody.current.classList.add("textRotate");
-
-  //   range != display ? setDisplay(display + 1) : setDisplay(0);
-  //   oldMain.current.classList.remove("vanish");
-  //   setTimeout(() => {
-  //     newMain.current.classList.add("hide");
-  //     textBody.current.classList.remove("textRotate");
-  //     // setTimeout(() => {
-  //     //   range != display ? setDisplay(display + 1) : setDisplay(0);
-  //     //   oldMain.current.classList.remove("vanish");
-  //     // }, 500)
-      
-  //     }, 4000)
-    
-
-  // }
 
   return (
     <div className="flexColumn">
@@ -153,17 +129,23 @@ const Portfolio = () => {
           <img className="mainImg opacityFilter" src={projects[display].image} alt={projects[display].alt} />
         </figure>
         
-        <div ref={textBody} className="flexColumn">
+        <div ref={textBody} className="descDiv flexColumn">
           <style>{textAnimation}</style>
-          <h3 className="subtitle">{projects[display].projectTitle}</h3>
-          <button href={projects[display].href}>Deployed Site</button>
-          <button href={projects[display].href}>Github Repository</button>
-          <p>{projects[display].desc}</p>
+          <h3 className="subtitle lineBreak">{projects[display].projectTitle}</h3>
+          <p className="lineBreak description">{projects[display].desc}</p>
+          <p className="lineBreak description">{projects[display].purpose}</p>
+          <div className="flexRow btnDisplay">
+            <button className="portfolioBtns" onClick={() => {window.open(projects[display].href, "_blank")}}>Deployed Site</button>
+            <button className="portfolioBtns" onClick={() => {window.open(projects[display].repo, "_blank")}}>Github Repository</button>
+          </div>
         </div>
       </div>
-      <div className="flexRow">
-        <button onClick={() => {0 != display ? setDisplay(display - 1) : setDisplay(range)}}> {'<'} </button>
-        <button onClick={() => {range != display ? setDisplay(display + 1) : setDisplay(0)}}> {'>'} </button>
+      <div className="flexRow iconDiv">
+        <button className="leftArrowBtn" onClick={() => {0 != display ? setDisplay(display - 1) : setDisplay(range)}}> <img className="arrow" src={Arrow}></img> </button>
+        <img className="iconFadeLeft" src={0 != display ? projects[display - 1].icon : projects[range].icon}></img>
+        <img className="icon" src={projects[display].icon} ></img>
+        <img className="iconFadeRight" src={range != display ? projects[display + 1].icon : projects[0].icon}></img>
+        <button className="rightArrowBtn" onClick={() => {range != display ? setDisplay(display + 1) : setDisplay(0)}}> <img className="arrow" src={Arrow}></img> </button>
       </div>
     </div>
     // <div className="testTwo"></div>
